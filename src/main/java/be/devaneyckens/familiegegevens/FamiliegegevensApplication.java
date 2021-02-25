5dc17bffc543fe9ceaaf2e3a1876a5fe6179c31b
Arne <a.vaneycken@gmail.com>
2021-02-25 06:04:17 +0100

    Initial commit

---
+++ src/main/java/be/devaneyckens/familiegegevens/FamiliegegevensApplication.java
@@ -0,0 +1,13 @@
+package be.devaneyckens.familiegegevens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliegegevensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamiliegegevensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/devaneyckens/familiegegevens/FamiliegegevensApplicationTests.java
@@ -0,0 +1,13 @@
+package be.devaneyckens.familiegegevens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliegegevensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
